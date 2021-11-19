59b111be5c0db4a6a36be736145de68d8470c2bc
zulutkachenko <zulutkachenko@gmail.com>
2021-11-19 17:31:30 +0200

    lesson-1

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+const cart = function() {
+    const cartBtn = document.querySelector('.button-cart');
+    const cart = document.getElementById('modal-cart');
+    const cartClose = cart.querySelector('.modal-close');
+
+    console.log(cart);
+
+    cartBtn.addEventListener('click', function() {
+        cart.style.display = 'flex';
+    });
+
+    cartClose.addEventListener('click', function() {
+        cart.style.display = '';
+    });
+}
+
+cart();
